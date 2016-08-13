3ae082bc879fa809163b7c3c644037d0631eacb7
Mam Wow <Mam Wow>
2016-08-13 12:28:39 +0100

    found out how to parse JSON

--- app/models.py
+++ app/models.py
@@ -7,6 +7,20 @@ from django.contrib.postgres.fields import JSONField
 class Food(models.Model):
     name = models.CharField('Name', max_length=200)
     ndbno = models.IntegerField('USDA Nutritional Database Number')
-    nut = JSONField()#db_tablespace='table'
+    nuts = JSONField()
     def __str__(self):  # __unicode__ on Python 2
         return self.name
+
+
+
+'''
+nutrients = Food.objects.get(whatever).nuts
+
+
+nutrient = filter(lambda n: n.get('name') == 'Manganese, Mn', nutrients)'
+
+
+
+nutrient_value = nutrient.get('value')
+nutrient_unit =
+'''
